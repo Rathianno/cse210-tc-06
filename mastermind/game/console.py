07ac333cc00acee2898b35bda0ffd759bcee5293
MagicWarrior494 <58752281+MagicWarrior494@users.noreply.github.com>
2021-03-18 12:38:11 -0600

    Console input and output

--- mastermind/game/console.py
+++ mastermind/game/console.py
@@ -1,3 +1,21 @@
 class console():
 
-    def 
+    def display_current_guesses(self,  player, player_guess, hint):
+        print("Player" + player + ": " + str(player_guess) + "," + hint)
+    
+    def get_player_guess(self, player):
+        print(player + "'s turn: ")
+        guessdoesntwork = True
+            while guessdoesntwork:
+                guess = input("What is your guess? ")
+                try:
+                    guess = int(guess)
+                except ValueError:
+                    print("Make sure you type a number")
+                    continue
+
+                if(guess < 0 or guess > 9999):
+                    print("Type a number between 0 and 9999")
+                    continue
+                guessdoesntwork = False
+        return result
